4bb7310bbc868180e0cf04e9d85a7411fde97375
DamianMoralesM <damian_morales_utn@hotmail.com>
2018-03-07 08:56:57 -0300

    Add files via upload

---
+++ js/animations.js
@@ -0,0 +1,36 @@
+window.sr = ScrollReveal();
+        sr.reveal('.test', {
+          duration: 2000,
+          origin:'bottom',
+          rotate: { x: 3, y: 69, z: 7 }
+          
+        });
+
+sr.reveal('.box', { duration: 2000 }, 50);
+
+sr.reveal('.showcase-right', {
+          duration: 2000,
+          origin:'right',
+          distance:'300px'
+        });
+
+ sr.reveal('.showcase-left', {
+          duration: 2000,
+          origin:'left',
+          distance:'300px'
+        });
+
+ sr.reveal('.sub', {
+          duration: 2000,
+          origin:'left',
+          distance:'300px',
+          delay: 0.3
+          
+        });
+
+         sr.reveal('.sobremi', {
+          duration: 2000,
+          origin:'bottom',
+         
+          
+        });
